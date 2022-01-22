dab52bb52c6c2413fa7d3e23acbe1fd12d8377e7
Thomas <thomas@boostcommerce.net>
2022-01-22 15:29:57 +0700

    Feat: Implement react-particles for UI

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,14 +1,101 @@
 import React from "react";
 import HeaderMobile from "./header-mobile";
 import HeaderDesktop from "./header-desktop";
+import styles from "@/styles/common.module.scss";
+import Particles from "react-tsparticles";
 
 interface HeaderProps {}
 
 export const Header = (props: HeaderProps) => {
   return (
-    <>
+    <div className={styles["header-wrapper"]}>
+      <div className={styles["particles-wrapper"]}>
+        <Particles
+          id="tsparticles"
+          height="200px"
+          width="100%"
+          options={{
+            fullScreen: { enable: false },
+            background: {
+              color: {
+                value: "transparent",
+              },
+            },
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 2,
+                },
+                repulse: {
+                  distance: 80,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#000",
+              },
+              links: {
+                color: "#000",
+                distance: 100,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 100,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      </div>
       <HeaderMobile />
       <HeaderDesktop />
-    </>
+    </div>
   );
 };
